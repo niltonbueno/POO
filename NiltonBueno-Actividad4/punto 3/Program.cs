e342054ce8b98776d6160457e7850ef761f5949f
nilton bueno <lionil1901@gmail.com>
2026-03-26 23:50:59 -0300

    Add files via upload

---
+++ NiltonBueno-Actividad4/punto 3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Advanced;
+
+namespace punto_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 3. Realizar un programa que acumule (sume) valores ingresados por teclado hasta
+            // ingresar el 9999(no sumar dicho valor, indica que ha finalizado la carga).Imprimir el
+            // valor acumulado e informar si dicho valor es cero, mayor a cero o menor a cero.
+
+            int  x, n;
+            n =     0;
+            string numero;
+
+            do
+            {
+                Console.Write("ingrese un numero: ");
+                numero = Console.ReadLine();
+                x = int.Parse(numero);
+
+                if (x != 9999)
+                {
+                    n = n + x;
+                }
+
+
+
+            }
+            while (x != 9999);
+
+            Console.Write("el total es: ");
+            Console.WriteLine(n);
+
+            if (n > 0)
+            {
+                Console.Write("es mayor a 0");
+
+            }
+            else
+            {
+                if (n < 0)
+                {
+                    Console.Write("es menor a 0");
+
+                }
+                else
+                {
+                                Console.Write("el igual que cero ");
+
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ NiltonBueno-Actividad4/punto 4/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4. En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada
+            // cuenta corriente se conoce: número de cuenta y saldo actual. El ingreso de datos debe
+            // finalizar al ingresar un valor negativo en el número de cuenta.
+            // Se pide confeccionar un programa que lea los datos de las cuentas corrientes e
+            // informe:
+            // a) De cada cuenta: número de cuenta y estado de la cuenta según su saldo, sabiendo
+            // que:
+            // Estado de la cuenta:
+            // ○ “Acreedor” si el saldo es &gt; 0.
+            // ○ “Deudor” si el saldo es &lt; 0.
+            // ○ “Nulo” si el saldo es = 0.
+            // b) La suma total de los saldos acreedores.
+
+
+
+            int x;
+            string valor, estado;
+            float saldo, sumaAcreedores;
+            sumaAcreedores = 0; 
+            do
+            {
+                Console.Write("ingrese su numero de cuenta: ");
+                valor = Console.ReadLine(); 
+                x = int.Parse(valor);
+
+                if (x >= 0)
+                {
+                    Console.Write("ingrese su saldo: ");
+                    valor = Console.ReadLine();
+                    saldo = float.Parse(valor);
+                    if (saldo > 0)
+                    {
+                        estado = "acreedor";
+                        sumaAcreedores = sumaAcreedores + saldo;
+                        Console.Write("el estado es: ");
+                        Console.WriteLine(estado);
+                    }
+                    else
+                    {
+                        if (saldo < 0)
+                        {
+                            estado = "deudor";
+                            Console.Write("el estado es: ");
+                            Console.WriteLine(estado);
+                        }
+                        else
+                        {
+                            estado = "nulo";
+                            Console.Write("el estado es: ");
+                            Console.WriteLine(estado);
+                        }
+                    }
+
+
+
+
+
+                }
+
+            }
+            while (x >= 0);
+            Console.Write("la suma total de los acreedores es: ");
+            Console.WriteLine(sumaAcreedores);
+
+        }
+    }
+}
