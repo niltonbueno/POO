4d56933cb909e3a82768da10f1cb8eee6b056075
nilton bueno <lionil1901@gmail.com>
2026-04-09 12:46:50 -0300

    Add files via upload

---
+++ NiltonBueno-Actividad 7/punto 1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_1
+{
+    internal class Program
+    {
+        public class Valores
+        {
+            private int[] elementos;
+            private int resultado;
+           
+            public void cargar()
+            {
+                elementos = new int[8];
+
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Console.Write("ingrese un valor: ");
+                    string numero;
+                    numero = Console.ReadLine();
+                    elementos[i] = int.Parse(numero);
+                }
+
+            }
+            public void acumulacion() 
+            { int suma;
+              suma = 0;
+                for (int f = 0; f < 8; f++)
+                {
+                    suma = suma + elementos[f];     
+
+                }
+                resultado = suma;
+
+                Console.Write("el resultado seria: ");
+                Console.WriteLine(resultado);
+            }
+            public void diferencia()
+            {
+                int valor1;
+                valor1 = 0;
+
+                int valor2;
+                valor2 = 0;
+
+                for (int g = 0; g < 8; g++)
+                {
+                    if (elementos[g] > 36 && elementos[g] < 50)
+                    {
+                      
+                        valor1 += 1;
+                    }
+                    else
+                    {
+                        if (elementos[g] >= 50)
+                        {
+                            
+                            valor2 += 1;
+                        }
+                    }
+                }
+
+                Console.Write("hay mayores a 36: ");
+                Console.WriteLine(valor1);
+                
+                
+                Console.Write("hay mayores a 50: ");
+                Console.WriteLine(valor2);
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            Valores vl1 = new Valores();
+            vl1.cargar();
+            vl1.acumulacion();
+            vl1.diferencia();
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ NiltonBueno-Actividad 7/punto 3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_3
+{
+    internal class Program
+    {
+        public class jugador
+        {
+            private int[] puntuacion;
+
+            public void carga ()
+            {
+                puntuacion = new int[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.Write($"ingre el puntaje de partida {i}: ");
+                    string numero = Console.ReadLine();
+                    puntuacion[i] = int.Parse(numero);
+                }
+            }
+            public void diferencia() 
+            {
+                int max, min;
+                max = 0;
+                min = 0;
+                for (int f = 0; f < 6; f++)
+                {
+
+                    if (puntuacion[f] > max)
+                    {
+                        max = puntuacion[f];
+                    }
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            jugador points = new jugador();
+            points.carga();
+
+
+
+
+
+
+        Console.ReadKey();
+        }
+    }
+}
