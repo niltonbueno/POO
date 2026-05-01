bc43b54fa709816fe54363fe353e7afe6914e913
nilton bueno <lionil1901@gmail.com>
2026-04-30 23:21:46 -0300

    Add files via upload

---
+++ NiltonBueno-Actividad 6/punto1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    internal class plano
+    {
+       
+            private int y, x;
+            private string number;
+
+            /*1.    Desarrollar una clase que represente un punto en el plano y tenga los
+                    siguientes métodos: cargar los valores de x e y, imprimir en que cuadrante se
+                    encuentra dicho punto (concepto matemático, primer cuadrante si x e y son
+                    positivas, si x< 0 e y >0 segundo cuadrante, etc.)*/
+
+
+            public void valores()
+            {
+                Console.Write("ingrese x: ");
+                number = Console.ReadLine();
+                x = int.Parse(number);
+                Console.Write("ingrese y: ");
+                number = Console.ReadLine();
+                y = int.Parse(number);
+            }
+            public void punto()
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.Write("estas en el primer cuadrante");
+                }
+                else
+                {
+                    if (x < 0 && y > 0)
+                    {
+                        Console.Write("estas en el segundo cuadrante");
+                    }
+                }
+            }
+        
+        static void Main(string[] args)
+        {
+            plano volor1 = new plano();
+            volor1.valores();
+            volor1.punto();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ NiltonBueno-Actividad 6/punto3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto3
+{
+    internal class empleado
+    {
+       
+            private int sueldo;
+            private string valor, name;
+
+            /*3. Confeccionar una clase que represente un empleado. Definir como atributos su
+                 nombre y su sueldo. Confeccionar los métodos para la carga, otro para imprimir
+                 sus datos y por último uno que imprima un mensaje si debe pagar impuestos
+                 (si el sueldo supera a 3000).*/
+
+            public void datos()
+            {
+                Console.Write("ingrese su nomber: ");
+                name = Console.ReadLine();
+                
+
+                Console.Write("ingrese su sueldo: ");
+                valor = Console.ReadLine();
+                sueldo = int.Parse(valor);
+            }
+
+            public void imprimir() 
+            {
+                Console.Write(" su nombre es: ");
+                Console.WriteLine(name);
+
+                Console.Write(" su sueldo es: ");
+                Console.WriteLine(sueldo);
+            }
+            public void impuestos ()
+            {
+                if (sueldo > 3000)
+                {
+                    Console.Write(" debe pagar impuestos");
+                }
+
+
+
+
+            }
+
+        
+        static void Main(string[] args)
+        {
+            empleado datos1 = new empleado();
+            datos1.datos();
+            datos1.imprimir();
+            datos1.impuestos();
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
