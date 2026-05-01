6116f9bad226dafd6e332456a4b093d869e99abc
nilton bueno <lionil1901@gmail.com>
2026-04-30 23:46:18 -0300

    Add files via upload

---
+++ Nilton Bueno-Actividad 8/punto 3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_3
+{
+    internal class vectores
+    {
+        /*3. Cargar un vector de n elementos de tipo entero. Ordenar posteriormente el vector
+             (el orden lo pueden elegir ustedes).*/
+        
+            int[] valores;
+            int numero;
+            int menor;
+            public void cargar () 
+            {
+                Console.Write("ingrese la cantidad de valores: ");
+                string numeros = Console.ReadLine();
+                numero = int.Parse(numeros);
+
+                valores = new int[numero];
+
+                for (int i = 0; i < valores.Length; i++)
+                {
+                    Console.Write("ingrese un valor entero: ");
+                    numeros = Console.ReadLine();
+                    valores[i] = int.Parse(numeros);
+                }
+            }
+            public void diferencia() 
+            {
+                Array.Sort(valores);
+            }
+            public void imprimir ()
+            {
+                for (int g = 0; g < valores.Length; g++)
+                {
+                    Console.WriteLine(valores[g]);
+
+
+
+
+                }
+
+
+
+            }
+        
+        
+        static void Main(string[] args)
+        {
+            vectores vectores = new vectores();
+            vectores.cargar();
+            vectores.diferencia();
+            vectores.imprimir();
+
+
+
+        }
+    }
+}
---
+++ Nilton Bueno-Actividad 8/punto 4/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto_4
+{
+    internal class nombre
+    {
+        /*4. Cargar en un vector los nombres de 5 países y en otro vector paralelo la cantidad de
+            habitantes de este. Ordenar alfabéticamente e imprimir los resultados. Por último
+            ordenar con respecto a la cantidad de habitantes (de mayor a menor) e imprimir
+            nuevamente.*/
+
+
+        
+            string[] paises;
+            int[] abitantes;
+
+            public void cargar()
+            {
+
+                paises = new string[5];
+                abitantes = new int[5];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("agrege un pais: ");
+                    string nombre = Console.ReadLine();
+                    paises[i] = nombre;
+
+                    Console.Write("agrege la cantidad de habitantes: ");
+                     nombre = Console.ReadLine();
+                    abitantes[i] = int.Parse(nombre);
+
+                }
+            }
+            public void alfavetico ()
+            {
+                //Array.Sort(paises);
+                //Array.Sort(abitantes);
+                for (int f = 0; f < 5; f++)
+                {
+                    for (int i = f + 1; i < 5; i++)
+                    {
+                        if (paises[f].CompareTo(paises[i]) < 0)
+                        {
+                            string aux;
+                            aux = paises[i];
+                            paises[i] = paises[f];
+                            paises[f] = aux;
+                            int auxAbit;
+                            auxAbit = abitantes[i];
+                            abitantes[i] = abitantes[f];
+                            abitantes[f] = auxAbit;
+                        }
+                    }
+                }
+            }
+            public void nomerico()
+            {
+                for (int f = 0; f < 5; f++)
+                {
+                    for (int i = f + 1; i < 5; i++)
+                    {
+                        if (abitantes[f] < abitantes[i])
+                        {
+                            int auxs;
+                            auxs = abitantes[f];
+                            abitantes[f] = abitantes[i];
+                            abitantes[i] = auxs;
+                            string auxPas;
+                            auxPas = paises[f];
+                            paises[f] = paises[i];
+                            paises[i] = auxPas;
+                        }
+                    }
+                }
+
+
+            }
+
+
+            public void imprimir() 
+            {
+                alfavetico();
+
+                    alfavetico();
+
+                    Console.WriteLine("Ordenado alfabetica");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(paises[i] + ": " + abitantes[i] + " Habitantes");
+
+
+                    }
+                    nomerico();
+
+                    Console.WriteLine("Ordenado numericamente");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(paises[i] + ": " + abitantes[i] + " Habitantes");
+
+
+
+                    }
+               
+            
+            
+            }
+
+        
+
+        static void Main(string[] args)
+        {
+            nombre nom = new nombre();
+            nom.cargar();
+            nom.alfavetico();
+            nom.nomerico();
+            nom.imprimir();
+
+
+
+
+        }
+    }
+}
