8b0e59e6db52afa42471c68481da370d99723e43
nilton bueno <lionil1901@gmail.com>
2026-06-05 12:02:20 -0300

    Add files via upload

---
+++ NiltonBueno-Actividad 15/punto1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    class Persona
+    {
+        /*1. Confeccionar una clase Persona que tenga como atributos el nombre y la
+            edad (definir las propiedades para poder acceder a dichos atributos). Definir
+            un método para imprimirlos. Plantear una segunda clase Empleado que
+            herede de la clase Persona. Añadir un atributo sueldo ( y su propiedad) y el
+            método para imprimir su sueldo. Definir un objeto de la clase Persona y
+            llamar a sus métodos y propiedades. También crear un objeto de la clase
+            Empleado y llamar a sus métodos y propiedades.*/
+        private int edad;
+        private string nombre;
+        
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+
+        public int Edad
+        {
+            set {
+                edad = value;
+            }
+            get {  
+                return edad;
+            }
+        }
+
+        public void impr ()
+        {
+            Console.WriteLine("nombre: " + nombre + " edad: " + edad);
+
+        }
+    }
+
+    class Empleado : Persona
+    {
+        private float sueldo;
+        
+
+        public void imp ()
+        {
+            Console.WriteLine("a: " + sueldo);
+
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Edad=20;
+            persona.Nombre = "lom";
+            persona.impr();
+            
+            Empleado emp = new Empleado();
+            emp.Nombre = "la";
+            emp.Edad = 52;
+            emp.sueldo = 14000;
+            emp.impr();
+
+        }
+    }
+}
---
+++ NiltonBueno-Actividad 15/punto2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto2
+{
+    class producto
+    {
+        /*2. Plantear una clase Producto y otra clase Inventario.
+            La clase Producto debe tener como atributos privados el nombre, precio y
+            stock. Definir propiedades para acceder a estos atributos, asegurando que el
+            stock no pueda ser negativo y el precio sea mayor a cero.
+            La clase Inventario debe contener 3 objetos de la clase Producto. Definir un
+            método para mostrar todos los productos ordenados de menor a mayor en
+            base al precio, además, mostrar el producto más caro y más barato del
+            inventario.*/
+        private string nombre;
+        private float precio;
+        private int stock;
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value; 
+            }
+
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public float Precio
+        {
+            set {
+                if (precio > 0)
+                {
+                    precio = value;
+                } 
+            }
+
+            get { 
+                return precio; 
+            }
+        }
+
+        public int Stock
+        {
+            set
+            {
+                if (stock >= 0)
+                {
+                    stock = value;
+                }
+            }
+
+            get
+            {
+                return stock;
+            }
+        }
+    } 
+    class inventario
+    {
+        private producto cliente1, cliente2, cliente3;
+        
+        public inventario()
+        {
+        cliente1 = new producto();    
+        cliente2 = new producto();
+        cliente3 = new producto();
+
+
+
+
+
+
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
